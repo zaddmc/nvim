f4051d7ed74a130f082588bc8e11d04ec1132ac9
zaddmc <davidbk927@gmail.com>
2025-02-05 10:58:57 +0100

    idk

--- lua/zadd/plugins/codesnap.lua
+++ lua/zadd/plugins/codesnap.lua
@@ -1,13 +1,13 @@
 return {
 	"mistricky/codesnap.nvim",
-	build = "make build-generator",
-	version = "^1",
-	lazy = "true",
+	build = "make",
 	config = function()
 		require("codesnap").setup({
-			title = "no",
-			mac_windows_bar = false,
-			save_path = home("/screenshots/snap.png"),
+			mac_window_bar = false,
+			show_workspace = true,
+			bg_x_padding = 0,
+			bg_y_padding = 0,
+			save_path = "~/screenshots/snap.png",
 		})
 	end,
 }
