fcebc5de1d47ef8632e0bbd99c4dcac0dda0318c
zaddmc <davidbk927@gmail.com>
2025-04-22 09:13:28 +0200

    formatting and test plugin

--- lua/zadd/core/options.lua
+++ lua/zadd/core/options.lua
@@ -4,17 +4,17 @@ local opt = vim.opt
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
-opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 4       -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 4    -- 2 spaces for indent width
-opt.expandtab = true  -- expand tab to spaces
+opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 4 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
@@ -22,13 +22,10 @@ opt.cursorline = true -- highlight the current cursor line
 -- turn on termguicolors for nightfly colorscheme to work
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
-
--- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
@@ -37,10 +34,10 @@ opt.splitbelow = true -- split horizontal window to the bottom
 -- Recommended code width
 -- opt.colorcolumn = "80"
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "python" },
-    callback = function()
-        vim.cmd("setlocal cc=88")
-    end,
+	pattern = { "python" },
+	callback = function()
+		vim.cmd("setlocal cc=88")
+	end,
 })
 
 -- turn off swapfile
