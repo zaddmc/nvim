95cc446bda2ad46f15183639e968e2d68d2a485b
zaddmc <davidbk927@gmail.com>
2025-09-04 13:34:27 +0200

    Permissions fixed

--- lua/zadd/homebrown/substitution.lua
+++ lua/zadd/homebrown/substitution.lua
@@ -1,7 +1,7 @@
 return {
-	vim.keymap.set("n", "<leader>mf", function()
-		vim.ui.input({ prompt = "Enter something: " }, function(input)
-			print("You entered: " .. (input or ""))
-		end)
-	end),
+    vim.keymap.set("n", "<leader>mf", function()
+        vim.ui.input({ prompt = "Enter something: " }, function(input)
+            print("You entered: " .. (input or ""))
+        end)
+    end),
 }
