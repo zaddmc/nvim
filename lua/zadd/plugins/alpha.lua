29ceb767d502d4189ce351da791c46666471d906
zaddmc <davidbk927@gmail.com>
2025-10-31 13:39:06 +0100

    now the alpha can be changed and one more

--- lua/zadd/plugins/alpha.lua
+++ lua/zadd/plugins/alpha.lua
@@ -1,31 +1,42 @@
 return {
-	"goolord/alpha-nvim",
-	event = "VimEnter",
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
-		local extra_alphas = require("zadd.homebrown.alphas")
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+        local extra_alphas = require("zadd.homebrown.alphas")
 
-		if vim.loop.cwd():find("^/home/zadd/Programming/Advent_of_Code") then
-			dashboard.section.header.val = vim.split(extra_alphas.aoc:random(), "\n")
-		else
-			dashboard.section.header.val = vim.split(extra_alphas.general:random(), "\n")
-		end
+        if vim.loop.cwd():find("^/home/zadd/Programming/Advent_of_Code") then
+            dashboard.section.header.val = vim.split(extra_alphas.aoc:random(), "\n")
+        else
+            dashboard.section.header.val = vim.split(extra_alphas.general:random(), "\n")
+        end
 
-		-- Set menu
-		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
-		}
+        -- Set menu
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+            dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+            dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+            dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+            dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+            dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+        }
 
-		-- Send config to alpha
-		alpha.setup(dashboard.opts)
+        -- Send config to alpha
+        alpha.setup(dashboard.opts)
 
-		-- Disable folding on alpha buffer
-		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-	end,
+        -- Disable folding on alpha buffer
+        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+
+        vim.api.nvim_create_user_command("AlphaChange", function(opts)
+            dashboard.section.header.val = vim.split(extra_alphas.general.items[opts.args], "\n")
+            vim.cmd(":AlphaRedraw")
+        end, {
+            desc = "Change alpha",
+            nargs = 1,
+            complete = function()
+                return extra_alphas.general.item_opts
+            end,
+        })
+    end,
 }
